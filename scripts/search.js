004768259c0343cbf47d08776ae8ccfe830dce9d
Mate Gombkoto <mategombhu@gmail.com>
2022-08-02 23:34:38 +0200

    extra comments

--- scripts/search.js
+++ scripts/search.js
@@ -1,7 +1,9 @@
-// the search form
+// grabbing the elements
 
 const searchFormElement = document.getElementById("js-search");
 const searchBarElement = document.getElementById("js-search-bar");
+
+// search it at submit events
 
 searchFormElement.addEventListener("submit", (event) => {
 	event.preventDefault();
