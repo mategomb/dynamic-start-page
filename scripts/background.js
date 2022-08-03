8dcfe8a7343757446056ec9540eb95fc2a7ffd89
Mate Gombkoto <mategombhu@gmail.com>
2022-08-03 14:46:23 +0200

    readded the background image code

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,49 @@
 // dynamically changing background images
 
-const directory = "assets";
-const extension = "jpg";
+let imagePaths = { day: [], night: [] };
 
-const changeBackgroundElement = document.getElementById("js-changebg");
+function getImagePaths(timeOfDay, numberOfImages) {
+	const imagePathsArray = [];
+
+	for (let index = 1; index <= numberOfImages; index++) {
+		const directory = "assets";
+		const extension = "jpg";
+
+		const imagePath = `${directory}/bg-${timeOfDay}-${index}.${extension}`;
+
+		imagePathsArray.push(imagePath);
+	}
+
+	return imagePathsArray;
+}
+
+imagePaths.day = getImagePaths("day", 7);
+imagePaths.night = getImagePaths("night", 3);
+
+let previousIndex = null;
+
+function loadimage() {
+	function getRandomImage(timeOfDay) {
+		const getRandomIndex = () => Math.floor(Math.random() * imagePaths[timeOfDay].length);
+
+		let newIndex = getRandomIndex();
+
+		for (; newIndex === previousIndex; ) {
+			newIndex = getRandomIndex();
+		}
+
+		previousIndex = newIndex;
+
+		return imagePaths[timeOfDay][newIndex];
+	}
+
+	let hour = new Date().getHours();
+
+	let imageFile = hour >= 20 || hour <= 5 ? getRandomImage("night") : getRandomImage("day");
+
+	document.body.style.backgroundImage = `linear-gradient(rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05)), url(${imageFile})`;
+}
+
+loadimage();
+
+document.getElementById("js-changebg").addEventListener("click", loadimage);
